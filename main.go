8c89fab74c0421b392e9a0b837aa567ebc351ee8
Hao Nguyen <hao@haonguyen.tech>
2023-08-23 16:03:14 +0700

    feat: basic main to check for open weather api key

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	log.Print("hello")
+	log.Println("Welcome to Go Weather App CLI")
+	log.Println("This is a fun project create to help learn Go Lang and CLI tool")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error: failed to load env, please make sure you create valid .env file", err)
+	}
+
+	// openWeatherApiKey := os.Getenv("OPEN_WEATHER_API_KEY")
 }
