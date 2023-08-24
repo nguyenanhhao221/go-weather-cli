2e010e41f9b6fd073cfe7fa47a0cb438e28123bd
Hao Nguyen <hao@haonguyen.tech>
2023-08-24 20:56:12 +0700

    feat: basic input read of city from user

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"os"
 
@@ -11,6 +12,7 @@ func main() {
 	log.Println("Welcome to Go Weather App CLI")
 	log.Println("This is a fun project create to help learn Go Lang and CLI tool")
 
+	// Handle ENV for the Open weather api key
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error: failed to load env, please make sure you create valid .env file", err)
@@ -20,4 +22,16 @@ func main() {
 	if !isPresent {
 		log.Fatalln("Cannot find OPEN_WEATHER_API_KEY variable in .env file, please make sure you have set the valid api key")
 	}
+
+	log.Print("Input your city: ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cityInput := scanner.Text()
+	log.Printf("Your input: %s", cityInput)
 }
